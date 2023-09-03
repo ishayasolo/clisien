553bb989162ecd80288ba1003c26a39f2216dc32
ishayasolo <ishaya808@gmail.com>
2023-09-04 00:34:53 +0100

    initialise app: add salt generator function

---
+++ src/generateSalt.ts
@@ -0,0 +1,26 @@
+/**
+ * Generates a random salt for the user.
+ *
+ * A salt is a random value used in cryptography to enhance the security of password storage
+ * by making each user's password hash unique, even if two users have the same password.
+ *
+ * @param {number} [saltLength=32] - The length of the generated salt. Default is 32 characters.
+ * @returns {string} The generated random salt.
+ *
+ * @example
+ * const userSalt = generateSalt(); // Generates a random 32-character salt
+ * const customSalt = generateSalt(64); // Generates a random 64-character salt
+ */
+const generateSalt = (saltLength: number = 32): string => {
+	const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let salt = "";
+
+	for (let i = 0; i < saltLength; i++) {
+		const randomIndex = Math.floor(Math.random() * charset.length);
+		salt += charset[randomIndex];
+	}
+
+	return salt;
+}
+
+export default generateSalt;
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import generateSalt from './generateSalt'
+
+export { generateSalt }
